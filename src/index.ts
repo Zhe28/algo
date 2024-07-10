d59d62ae6dd258c13a81f5077f8a3dfd4e132283
Huang Zhe <3451701311@qq.com>
2024-07-10 23:17:58 +0800

    feat[单链表] : 完成基础的增删改查

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // 单项链表
-class ListNode {
+class listNode {
   val: number;
-  next: ListNode | null;
+  next: listNode | null;
 
   constructor(x: number) {
     this.val = x;
@@ -9,17 +9,17 @@ class ListNode {
   }
 }
 
-function createLinkedList(nums: number[]): ListNode | null {
+function createLinkedList(nums: number[]): listNode | null {
   if (nums === null || nums.length === 0) return null;
 
-  const head = new ListNode(nums[0]);
+  const head = new listNode(nums[0]);
   let current = head;
 
   for (let i = 1; i < nums.length; i++) {
-    current.next = new ListNode(nums[i]);
+    current.next = new listNode(nums[i]);
     current = current.next;
   }
   return head;
 }
 
-export { ListNode, createLinkedList };
+export { listNode, createLinkedList };
